6a46caa1f7b8c2e91d93d895ca50b850b54ea2f4
Shunsuke Tadokoro <shunsuke.tadokoro@bizreach.co.jp>
2017-02-15 15:58:28 +0900

    Parse request

--- src/main/java/io/github/todokr/HttpRequest.java
+++ src/main/java/io/github/todokr/HttpRequest.java
@@ -1,22 +1,36 @@
 package io.github.todokr;
 
-import java.io.InputStream;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HttpRequest {
 
     private final String method;
     private final String path;
     private final String httpVersion;
-    private final Map<String, String> headers;
+    private final Map<String, String> headers = new HashMap<>();
 
-    HttpRequest(InputStream input) {
-        // TODO
-        this.method = "GET";
-        this.path = "/index.html";
-        this.httpVersion = "1.1";
-        this.headers = new HashMap<String, String>();
+    HttpRequest(InputStream input) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new BufferedInputStream(input)));
+
+        String[] requestLineItems = reader.readLine().split("\\s+");
+        this.method      = requestLineItems[0];
+        this.path        = requestLineItems[1];
+        this.httpVersion = requestLineItems[2];
+
+        Pattern headerPattern = Pattern.compile("(?<key>\\S+)\\s*:\\s*(?<value>\\S+)");
+        String pair = reader.readLine();
+        while (pair != null || pair.isEmpty()) {
+            Matcher matcher = headerPattern.matcher(pair);
+            if(matcher.find()) {
+                this.headers.put(matcher.group("key"), matcher.group("value"));
+            }
+            pair = reader.readLine();
+        }
     }
 
     String getMethod() {
