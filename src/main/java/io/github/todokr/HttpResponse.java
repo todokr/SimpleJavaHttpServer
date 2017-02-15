47315bd0f96f93ab4375dd4965cc3eb029380da4
Shunsuke Tadokoro <shunsuke.tadokoro@bizreach.co.jp>
2017-02-15 16:04:11 +0900

    Respond Last-Modified

--- src/main/java/io/github/todokr/HttpResponse.java
+++ src/main/java/io/github/todokr/HttpResponse.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class HttpResponse {
 
@@ -54,12 +55,14 @@ public class HttpResponse {
             "HTTP/1.1 %s\r\n" +
             "Content-Type: %s\r\n" +
             "Content-Length: %d\r\n" +
+            "Last-Modified: %s\r\n" +
             "Server: Java Simple HTTP Server\r\n" +
             "Connection: Close\r\n" +
             "\r\n",
             this.status.statusCode,
             this.contentType,
-            this.contentLength);
+            this.contentLength,
+            DateTimeFormatter.RFC_1123_DATE_TIME.format(this.date));
 
         out.write(header.getBytes(StandardCharsets.UTF_8));
         out.write(this.body);
