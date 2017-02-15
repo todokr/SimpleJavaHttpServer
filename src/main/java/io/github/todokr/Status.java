6484f021bedec5aeab758ad2610962671670162e
Shunsuke Tadokoro <shunsuke.tadokoro@bizreach.co.jp>
2017-02-15 15:58:15 +0900

    clean up

--- src/main/java/io/github/todokr/Status.java
+++ src/main/java/io/github/todokr/Status.java
@@ -1,13 +1,13 @@
 package io.github.todokr;
 
-
 public enum Status {
     OK("200 OK"),
     NotFound("404 NotFound"),
     InternalServerError("500 InternalServerError");
 
     public final String statusCode;
-    private Status(String statusCode) {
+
+    Status(String statusCode) {
         this.statusCode = statusCode;
     }
 }
